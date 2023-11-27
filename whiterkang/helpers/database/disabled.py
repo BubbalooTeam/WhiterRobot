16b28de51fd637b4c818722130d6f531e7ddce15
Davidev <97841570+davitudoplugins1234@users.noreply.github.com>
2023-11-27 19:01:29 -0300

    @BubbalooTeam

--- whiterkang/helpers/database/disabled.py
+++ whiterkang/helpers/database/disabled.py
@@ -3,5 +3,5 @@ from whiterkang import db
 DISABLED = db["DISABLED"]
 
 async def is_disabled(gid: int, query: str) -> bool:
-    off = await DISABLED.find_one({"chat_id": gid, "_cmd": query})
+    off = await DISABLED.find_one({"chat_id": gid, "cmd": query})
     return bool(off)
