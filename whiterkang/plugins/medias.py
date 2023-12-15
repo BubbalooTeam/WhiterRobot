f80089ea72186c57d0ee33db4bdeb9c9d58390cd
Davitudoplugins1234 <kannaxuserbot@gmail.com>
2023-12-15 13:53:07 -0300

    @BubbalooTeam - print errors

--- whiterkang/plugins/medias.py
+++ whiterkang/plugins/medias.py
@@ -237,7 +237,7 @@ async def scroll_ytdl(c: WhiterX, cq: CallbackQuery):
     try: 
         key_search, user, pages, chat_id = cq.data.split("|")
     except ValueError:
-        return await c.send_log("Scroll ValueError in: {cq.data}")
+        return await c.send_log(f"Scroll ValueError in: {cq.data}")
     
     chat_id = int(chat_id)
     
