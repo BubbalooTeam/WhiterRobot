a851eca4bbb9fcf28fda18b4b35bd23ab0a99512
Davitudoplugins1234 <kannaxuserbot@gmail.com>
2023-12-16 13:55:22 -0300

    @BubbalooTeam

--- whiterkang/helpers/medias.py
+++ whiterkang/helpers/medias.py
@@ -503,6 +503,10 @@ async def get_download_button(format: str, yt_id: str, user_id: int, chat_id: in
                         )
                     )
             buttons += sublists(video_btns, width=2)
+            buttons += InlineKeyboardButton(
+                await tld(chat_id, "BACK_BNT"),
+                callback_data=f"yt_scroll|{key_search}|{user_id}|1|{chat_id}"
+            )
         if aud:
             buttons += best_audio_btn
             buttons += sublists(
@@ -517,12 +521,10 @@ async def get_download_button(format: str, yt_id: str, user_id: int, chat_id: in
                 width=2,
             )
 
-    buttons.append(
-        InlineKeyboardButton(
-            await tld(chat_id, "BACK_BNT"),
-            callback_data=f"yt_scroll|{key_search}|{user_id}|1|{chat_id}"
-        )
-    )
+            buttons += InlineKeyboardButton(
+                await tld(chat_id, "BACK_BNT"),
+                callback_data=f"yt_scroll|{key_search}|{user_id}|1|{chat_id}"
+            )
 
     return SearchResult(
         yt_id,
