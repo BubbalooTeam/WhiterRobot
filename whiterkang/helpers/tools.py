8ddffebf3fa27116b7999eadc207e6291846e01a
Davitudoplugins1234 <kannaxuserbot@gmail.com>
2023-12-27 23:56:03 -0300

    @BubbalooTeam - Fixes

--- whiterkang/helpers/tools.py
+++ whiterkang/helpers/tools.py
@@ -384,7 +384,7 @@ async def cssworker_url(target_url: str):
         res = requests.get(url)
         soup = BeautifulSoup(await res.text(), "html.parser")
         scl_secret = soup.findAll("input")[1]["value"]
-        key = md5((str(target_url) + scl_secret).encode()).hexdigest()
+        key = md5((target_url + scl_secret).encode()).hexdigest()
         resp = f"https://screenshotlayer.com/php_helper_scripts/scl_api.php?secret_key={key}&url={target_url}"
         return resp
     except HTTPError:
