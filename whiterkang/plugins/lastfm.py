0370166ed0a527ef0fe311c2cba83eafef638500
Davitudoplugins1234 <kannaxuserbot@gmail.com>
2023-12-29 13:26:23 -0300

    @BubbalooTeam

--- whiterkang/plugins/lastfm.py
+++ whiterkang/plugins/lastfm.py
@@ -230,7 +230,8 @@ async def now_play(c: WhiterX, m: Message):
     song_ = recent_song[0]
     song_name = song_["name"]
     artist_name = song_["artist"]["name"]
-    timestamp = int(recent_song["date"]["uts"])
+    timestamp = recent_song["date"]["uts"]
+    timestamp = int(timestamp)
     image_ = song_["image"][3].get("#text")
     params_ = {
         "method": "track.getInfo",
