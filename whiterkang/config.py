24ddd78f7f261044e873a54a9fccebd9043b4fd1
DaviDev <97841570+davitudoplugins1234@users.noreply.github.com>
2024-01-01 23:15:37 -0300

    Fixes

--- whiterkang/config.py
+++ whiterkang/config.py
@@ -25,7 +25,7 @@ class Config:
         5374262463, # @junglivre
         1715384854, # @DaviTudo 
         1574959180, # @KlonoaSulista
-        6419654672 # @lucmsilva
+        6419654672, # @lucmsilva
     )
     ADMINS = {}
     LASTFM_API_KEY = os.environ.get("LASTFM_API_KEY")
