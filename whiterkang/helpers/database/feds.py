f74d39ba546700abf5116bccce262846d5eb51d0
DaviDev <97841570+davitudoplugins1234@users.noreply.github.com>
2024-01-02 18:22:30 +0000

    @BubbalooTeam

--- whiterkang/helpers/database/feds.py
+++ whiterkang/helpers/database/feds.py
@@ -37,10 +37,10 @@ async def add_fed(fed_name, creator_id):
 async def add_fed_chat(fed_id, chat_id):
     return await DB_FEDS.update_one(
         {'_id': fed_id},
-        {"$addToSet": {'chats': {'$each': [chat_id]}}}
+        {"$addToSet": {'chats': {'$each': [chat_id]}}}, upsert=True
     )
 async def del_fed_chat(fed_id, chat_id):
     return await DB_FEDS.update_one(
         {'fed_id': fed_id},
-        {"$pull": {'chats': {'$each': [chat_id]}}}
+        {"$pull": {'chats': {'$each': [chat_id]}}}, upsert=True
     )
