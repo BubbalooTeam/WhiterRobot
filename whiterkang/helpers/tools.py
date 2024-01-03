bd5c7ad83a210a35f88c4fd2580f1b4bdfdd2bd1
DaviDev <97841570+davitudoplugins1234@users.noreply.github.com>
2024-01-03 18:18:31 +0000

    @BubbalooTeam

--- whiterkang/helpers/tools.py
+++ whiterkang/helpers/tools.py
@@ -739,7 +739,7 @@ async def quotify(messages: [Message], replied: bool):
                 {
                     "entities": [
                         {
-                            "type": entity_mapping[entity.type] if entity_mapping and not entity== None else "text",
+                            "type": entity_mapping[entity.type] if entity_mapping and not entity == None else "text",
                             "offset": entity.offset,
                             "length": entity.length,
                         }
