b143fa91a3ae5a7fc868ae579ab0e09dfb5fd687
DaviDev <97841570+davitudoplugins1234@users.noreply.github.com>
2024-01-03 15:33:45 +0000

    @BubbalooTeam

--- whiterkang/helpers/tools.py
+++ whiterkang/helpers/tools.py
@@ -706,87 +706,79 @@ async def format_plate_info(chat_id: int, info: dict) -> str:
 arq = ARQ("https://arq.hamker.dev", Config.ARQ_API_KEY, aiohttp.ClientSession())
 
 async def quotify(messages: [Message]):
-
     if not isinstance(messages, list):
         messages = [messages]
 
     json = {
-        "type": "quote",
-        "format": "png",
-        "backgroundColor": "#1c1c1c",
-        "messages": [
-            {
-                "entities": [
-                    {
-                        "type": entity.type,
-                        "offset": entity.offset,
-                        "length": entity.length,
+            "type": "quote",
+            "format": "png",
+            "backgroundColor": "#1b1429",
+            "messages": [
+                {
+                    "entities": [],
+                    "chatId": message.forward_from.id
+                    if message.forward_from
+                    else message.from_user.id,
+                    "avatar": True,
+                    "from": {
+                        "id": message.from_user.id,
+                        "username": message.from_user.username
+                        if message.from_user.username
+                        else "",
+                        "photo": {
+                            "small_file_id": message.from_user.photo.small_file_id,
+                            "small_photo_unique_id": message.from_user.photo.small_photo_unique_id,
+                            "big_file_id": message.from_user.photo.big_file_id,
+                            "big_photo_unique_id": message.from_user.photo.big_photo_unique_id,
+                        }
+                        if message.from_user.photo
+                        else "",
+                        "type": "private" if message.chat.type == ChatType.PRIVATE else "supergroup",
+                        "name": message.from_user.first_name,
                     }
-                    for entity in message.entities
-                ]
-                if message.entities
-                else [],
-                "chatId": message.forward_from.id
-                if message.forward_from
-                else message.from_user.id,
-                "avatar": True,
-                "from": {
-                    "id": message.from_user.id,
-                    "username": message.from_user.username
-                    if message.from_user.username
-                    else "",
-                    "photo": {
-                        "small_file_id": message.from_user.photo.small_file_id,
-                        "small_photo_unique_id": message.from_user.photo.small_photo_unique_id,
-                        "big_file_id": message.from_user.photo.big_file_id,
-                        "big_photo_unique_id": message.from_user.photo.big_photo_unique_id,
-                    }
-                    if message.from_user.photo
-                    else "",
-                    "type": message.chat.type,
-                    "name": message.from_user.first_name,
+                    if not message.forward_from
+                    else {
+                        "id": message.forward_from.id,
+                        "username": message.forward_from.username
+                        if message.forward_from.username
+                        else "",
+                        "photo": {
+                            "small_file_id": message.forward_from.photo.small_file_id,
+                            "small_photo_unique_id": message.forward_from.photo.small_photo_unique_id,
+                            "big_file_id": message.forward_from.photo.big_file_id,
+                            "big_photo_unique_id": message.forward_from.photo.big_photo_unique_id,
+                        }
+                        if message.forward_from.photo
+                        else "",
+                        "type": "private" if message.chat.type == ChatType.PRIVATE else "supergroup",
+                        "name": message.forward_from.first_name,
+                    },
+                    "text": message.text if message.text else "",
+                    "replyMessage": (
+                        {
+                            "name":
+                                message.reply_to_message.from_user.first_name,
+                            "text": message.reply_to_message.text,
+                            "chatId": message.reply_to_message.from_user.id,
+                        }
+                        if message.reply_to_message
+                        else {}
+                    )
+                    if len(messages) == 1
+                    else {},
                 }
-                if not message.forward_from
-                else {
-                    "id": message.forward_from.id,
-                    "username": message.forward_from.username
-                    if message.forward_from.username
-                    else "",
-                    "photo": {
-                        "small_file_id": message.forward_from.photo.small_file_id,
-                        "small_photo_unique_id": message.forward_from.photo.small_photo_unique_id,
-                        "big_file_id": message.forward_from.photo.big_file_id,
-                        "big_photo_unique_id": message.forward_from.photo.big_photo_unique_id,
-                    }
-                    if message.forward_from.photo
-                    else "",
-                    "type": message.chat.type,
-                    "name": message.forward_from.first_name,
-                },
-                "text": message.text if message.text else "",
-                "replyMessage": (
-                    {
-                        "name": 
-                        message.reply_to_message.from_user.first_name,
-                        "text": message.reply_to_message.text,
-                        "chatId": message.reply_to_message.from_user.id,
-                    }
-                    if message.reply_to_message
-                    else {}
-                )
-                if len(messages) == 1
-                else {},
-            }
-            for message in messages
-        ],
-    }
-    
+                for message in messages
+            ],
+        }
+
+    # Agora, vamos processar os dados e prepará-los para a solicitação
     response = requests.post('https://bot.lyo.su/quote/generate', json=json).json()
     buffer = base64.b64decode(response['result']['image'].encode('utf-8'))
+    buffer = BytesIO(buffer)
+    buffer.name = "sticker.webp"
 
-    sticker = BytesIO(buffer)
-    sticker.name = "sticker.webp"
-    return [True, sticker]
+    return [True, buffer]
+
 
 
 def isArgInt(m: Message) -> list:
